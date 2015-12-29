5084e5a85a4ab245ec15b291db133421017513a7
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-12-29 01:37:41 +0100

    Add full ESP8266 implementation

---
+++ src/esp8266/Esp8266AWSImplementations.cpp
@@ -0,0 +1,170 @@
+#include "DeviceIndependentInterfaces.h"
+#include "Esp8266AWSImplementations.h"
+
+#include <ESP8266WiFi.h>
+
+void printWifiStatus() {
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+}
+
+bool Esp8266_Wifi_Setup(char* pSSID, char* pPassword) {
+    int tempStatus = WL_IDLE_STATUS;
+
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(pSSID);
+
+    // If pPassword is set the most secure supported mode will be automatically selected
+    WiFi.begin(pSSID, pPassword);
+
+    // Wait for WiFi connection
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println();
+
+    return true;
+}
+
+
+int delayTime = 500;
+
+Esp8266HttpClient::Esp8266HttpClient() {
+}
+
+char* Esp8266HttpClient::send(const char* request, const char* serverUrl,
+        int port) {
+    /* Arduino String to build the response with. */
+    String responseBuilder = "";
+    if (client.connect(serverUrl, port)) {
+        /* Send the requests */
+        client.println(request);
+        client.println();
+        /* Read the request into responseBuilder. */
+        delay(delayTime);
+        while (client.available()) {
+            char c = client.read();
+            responseBuilder.concat(c);
+        }
+        client.stop();
+    } else {
+        client.stop();
+        /* Error connecting. */
+        return 0;
+    }
+    /* Copy responseBuilder into char* */
+    int len = responseBuilder.length();
+    char* response = new char[len + 1]();
+    responseBuilder.toCharArray(response, len + 1);
+    return response;
+}
+
+bool Esp8266HttpClient::usesCurl() {
+    /* Does not use curl command. */
+    return false;
+}
+
+Esp8266DateTimeProvider::Esp8266DateTimeProvider() {
+    /* These are initialized to defaults. The defaults will cause the request
+     * to fail, but the AWS Services response will contain the correct time
+     * which will be fed to sync(). */
+    year = 2000;
+    month = 1;
+    day = 1;
+    hour = 0;
+    minute = 0;
+    second = 0;
+
+    syncedTime = 0;
+}
+
+const char* Esp8266DateTimeProvider::getDateTime() {
+    const int secondsPerMinute = 60;
+    const int secondsPerHour = secondsPerMinute * 60;
+    const long int secondsPerDay = secondsPerHour * 24;
+    /* Determine how many seconds have passed since the last sync, break those
+     * seconds down into days/hours/minutes/seconds, then form the new time
+     * string. */
+    long int newSecond = ((millis() - syncedTime) / 1000) + second
+            + (secondsPerMinute * minute) + (secondsPerHour * hour)
+            + (secondsPerDay * day);
+    int newDay = newSecond / secondsPerDay;
+    newSecond = newSecond % secondsPerDay;
+    int newHour = newSecond / secondsPerHour;
+    newSecond = newSecond % secondsPerHour;
+    int newMinute = newSecond / secondsPerMinute;
+    newSecond = newSecond % secondsPerMinute;
+
+    /* Without cumbersome data about how many days a month we cannot factor the
+     * extra time into more months. AWS Services give different errors that do
+     * not provide us with the current time if the date is invalid rather than
+     * innacurate. However, going past the number of days in a month (i.e
+     * giving 30 for February) is valid, so long as the days does not exceed
+     *  31. */
+    newDay = newDay > 31 ? 31 : newDay;
+
+    /* Copy the values to dateTime and return it. */
+    sprintf(dateTime, "%04d%02d%02d%02d%02d%02d", year, month, newDay, newHour,
+            newMinute, newSecond);
+
+    return dateTime;
+}
+
+bool Esp8266DateTimeProvider::syncTakesArg(void) {
+    return true;
+}
+
+void Esp8266DateTimeProvider::sync(const char* dateTime) {
+    /* If given a null dateTime, do nothing. */
+    if (dateTime) {
+        /* Set synced time to the number of milliseconds since the sketch
+         * started. */
+        syncedTime = millis();
+        /* Copy each substring into the buffer then convert it to an integer. */
+        char buffer[5];
+        // Year
+        strncpy(buffer, dateTime, 4);
+        year = atoi(buffer);
+        memset(buffer, '\0', 5);
+        // Month
+        strncpy(buffer, dateTime + 4, 2);
+        month = atoi(buffer);
+        memset(buffer, '\0', 5);
+        // Day
+        strncpy(buffer, dateTime + 6, 2);
+        day = atoi(buffer);
+        memset(buffer, '\0', 5);
+        // Hour
+        strncpy(buffer, dateTime + 8, 2);
+        hour = atoi(buffer);
+        memset(buffer, '\0', 5);
+        // Minute
+        strncpy(buffer, dateTime + 10, 2);
+        minute = atoi(buffer);
+        memset(buffer, '\0', 5);
+        // Second
+        strncpy(buffer, dateTime + 12, 2);
+        second = atoi(buffer);
+        memset(buffer, '\0', 5);
+        
+        /* If these values come out as 0, change them to valid AWS date values
+         * (i.e. cannot have month 0). */
+        year = year ? year : 2000;
+        month = month ? month : 1;
+        day = day ? day : 1;
+    }
+}
