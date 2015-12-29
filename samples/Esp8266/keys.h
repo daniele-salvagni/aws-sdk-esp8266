ec5d7637735c3a9fdc57e1d48724af12f6ebaf16
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-12-29 14:50:03 +0100

    Leave only keys in key files

--- samples/Esp8266/keys.h
+++ samples/Esp8266/keys.h
@@ -1,19 +1,7 @@
-// keys.h
 #ifndef KEYS_H_
 #define KEYS_H_
 
-extern const char* awsKeyID;
-extern const char* awsSecKey;
-
-extern const char* AWS_REGION;
-extern const char* AWS_ENDPOINT;
-
-extern char* pSSID;
-extern char* pPassword;
-
-extern const char* TABLE_NAME;
-extern const char* HASH_KEY_NAME;
-extern const char* HASH_KEY_VALUE;
-extern const char* RANGE_KEY_NAME;
+extern const char* awsKeyID;  // Declare these variables to 
+extern const char* awsSecKey; // be accessible by the sketch
 
 #endif
